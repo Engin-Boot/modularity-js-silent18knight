85c0ff42bbf750561463dbc4b91beb449880ba9a
Himanshu Chaudhary <45047527+silent18knight@users.noreply.github.com>
2020-08-25 13:01:11 +0530

    modularity added

---
+++ colors.js
@@ -0,0 +1,17 @@
+const MajorColors = [
+    "white",
+    "red",
+    "black",
+    "yellow",
+    "violet"
+];
+
+const MinorColors = [
+    "blue",
+    "orange",
+    "green",
+    "brown",
+    "slate"
+];
+
+module.exports={MajorColors,MinorColors};
